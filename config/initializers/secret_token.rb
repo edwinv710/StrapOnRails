980729aa13560be522cafb510b3b6e40de875a82
Edwin Velasquez <ev9@buffalo.edu>
2013-02-18 21:00:57 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrapOnRails::Application.config.secret_token = 'f00091cb9abb25e632201d80bc35616fdb28f171504052d370e1da76d298aec72e9aa8ee01a93c5ef3984e92f5c53af87d63091be97feeba2b0dc10abeefe49a'
